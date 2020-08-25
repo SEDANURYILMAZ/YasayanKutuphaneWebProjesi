ef28e83ac6bd2e8533f21b41238672ed384dc6e3
SEDANURYILMAZ <sedanur-y97@hotmail.com>
2020-08-25 19:53:31 +0300

    Projede Yasayan Kütüphane isimli bir web site tasarladım. Sitenin genel amacı kullanıcılara kitap katologu sunmak. Sitede giriş yaparken ilk olarak kitaplarımızın listelendiği bir sayfayla karşı karşıya kalacaksınız. Bu sayfada sizin ilginizi çekecek kitaplara yorum yapabilirsiniz.
    Yasayan Kutuphane

---
+++ YasayanKutuphane/Pages/Customer/Home/Index.cshtml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Localization;
+
+namespace YasayanKutuphane.Pages.Customer.Home
+{
+    public class IndexModel : PageModel
+    {
+        private readonly YasayanKutuphane.Data.ApplicationDbContext _db;
+        public IndexModel(YasayanKutuphane.Data.ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        /*private readonly IStringLocalizer<IndexModel> _localizer;
+        public IndexModel(IStringLocalizer<IndexModel> localizer)
+        {
+            _localizer = localizer;
+        }
+        */
+        public List<YasayanKutuphane.Models.Kitap> Satista { get; set; }
+        public List<YasayanKutuphane.Models.Kitap> CokYakinda { get; set; }
+        public void OnGet()
+        {
+            Satista = _db.Kitap.ToList();
+
+        }
+        public ActionResult DilSecim(string dil)
+        {
+            return Page();
+        }
+    }
+}
